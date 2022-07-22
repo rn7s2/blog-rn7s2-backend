57af0a3655b7451200a4d7d11f23ea23e143fd78
rn7s2 <mail@rn7s2.cn>
2022-07-23 00:18:53 +0800

    Add DbContext.

--- blog-rn7s2-backend/Models/Page.cs
+++ blog-rn7s2-backend/Models/Page.cs
@@ -2,6 +2,8 @@
 {
     public class Page
     {
+        // “导航属性”，需要阅读更多文档来确认
+        public int PageId { get; set; } = 0;
         public string Name { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public DateTime Updated { get; set; } = DateTime.MinValue;
